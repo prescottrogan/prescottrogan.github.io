c7a2e198ec0effe90ac1610c0c2040b870291129
Rogan Prescott <prescottrogan@gmail.com>
2019-11-04 14:51:57 -0800

    Connected website to links that i forgot to share.

--- CS160/processing/sketch_two/sketch.js
+++ CS160/processing/sketch_two/sketch.js
@@ -1,7 +1,44 @@
+var x=0;
+var y=0;
+var px=0;
+var py=0;
+var easing = .05;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  // put setup code here
+  cnv = createCanvas(800, 400);
+  centerCanvas();
+  background(32, 32, 32);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function randomColor() {
+  var a = random(0, 255);
+  var b = random(0, 255);
+  var c = random(0, 255);
+  background(a, b, c);
 }
 
 function draw() {
-  // put drawing code here
+  var targetX = mouseX;
+  x += (targetX - x) * easing;
+  var targetY = mouseY;
+  y += (targetY - y) * easing;
+  var weight = dist(x, y, px, py);
+  strokeWeight(weight);
+  line(x, y, px, py);
+  py = y;
+  px = x;
+}
+
+function mousePressed() {
+  randomColor();
 }

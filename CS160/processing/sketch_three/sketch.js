109a39e7181d124f3ddc0a0bb27e69bfc4fea137
Rogan Prescott <prescottrogan@gmail.com>
2019-12-12 17:33:42 -0800

    updated links

--- CS160/processing/sketch_three/sketch.js
+++ CS160/processing/sketch_three/sketch.js
@@ -1,7 +1,31 @@
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
 function setup() {
-  // put setup code here
+  cnv = createCanvas(800, 400);
+  centerCanvas();
+  background(32, 32, 32);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function randomColor() {
+  var a = random(0, 255);
+  var b = random(0, 255);
+  var c = random(0, 255);
+  background(a, b, c);
 }
 
 function draw() {
-  // put drawing code here
+  var d = random(0, 255);
+  var e = random(0, 255);
+  var f = random(0, 255);
+  fill(d,e,f);
+  noStroke();
+  ellipse(mouseX, mouseY, 25, 25);
 }
